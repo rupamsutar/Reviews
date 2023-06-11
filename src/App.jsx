a35a2b26a88a0d376a0fdd6efe93b2af34b6b96c
Rupam Sutar <rupamsutar281998@gmail.com>
2023-06-11 19:45:39 +0530

    react-icons setup completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import { useState } from "react";
-import people from "./data"
+import people from "./data";
+import {FaBeer} from "react-icons/fa";
 
 const App = () => {
   const [index, setIndex] = useState(0)
 
   const {name, job, image, text} = people[index];
 
-  return <h2>Reviews Starter</h2>;
+  return(
+    <div>
+    <h2>Reviews Starter</h2>
+    <FaBeer className="beer" />
+    </div>
+  ) 
 };
 export default App;
