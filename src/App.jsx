118d713c66074941c6cf83cfd0e61e2b1c00de44
Rupam Sutar <rupamsutar281998@gmail.com>
2023-06-11 20:11:07 +0530

    Bug Fixed

--- src/App.jsx
+++ src/App.jsx
@@ -6,16 +6,28 @@ const App = () => {
   const [index, setIndex] = useState(0);
   const {name, job, image, text} = people[index];
 
+  const checkNumber =(number) => {
+    if (number > people.length - 1) {
+      return 0
+    }
+
+    if (number < 0) {
+      return people.length - 1
+    }
+
+    return number;
+  }
+
   const nextPerson = () => {
     setIndex((currentIndex) => {
       const newIndex = currentIndex + 1;
-      return newIndex;
+      return checkNumber(newIndex);
     })
   };
   const prevPerson = () => {
     setIndex((currentIndex) => {
       const newIndex = currentIndex - 1;
-      return newIndex;
+      return checkNumber(newIndex);
     })
   };
 
